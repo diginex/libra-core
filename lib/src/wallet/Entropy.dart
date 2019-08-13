50c9d5228a603cd1436e673f1905a4cd02240653
mavis.tan <mavis.tan@diginex.com>
2019-08-13 10:44:06 +0800

    update entropy check
    limit to 64 (24 mnemonic word list)

--- lib/src/wallet/Entropy.dart
+++ lib/src/wallet/Entropy.dart
@@ -1,12 +1,12 @@
 import 'dart:math';
-import 'package:hex/hex.dart';
 import 'package:flutter_libra_core/src/LibraHelpers.dart';
+
+final int entropyLength = 64;
 
 class Entropy {
   /// Returns true if a entropy is valid, false otherwise
   static bool isValidEntropy(String entropyString) {
-    final entropy = HEX.decode(entropyString);
-    if (entropy.length < 16 || entropy.length > 32 || entropy.length % 4 != 0) {
+    if (entropyString == null || entropyString.length != entropyLength) {
       return false;
     }
     // Ensure entropy only contains hex characters, 0-9;A-F
@@ -17,7 +17,7 @@ class Entropy {
     String result = ''; // Resulting entropy when done
     String chars = 'abcdef0123456789'; // Characters a entropy may contain
     Random rng = Random.secure();
-    for (int i = 0; i < 64; i++) {
+    for (int i = 0; i < entropyLength; i++) {
       result += chars[rng.nextInt(chars.length)];
     }
     return result.toUpperCase();
